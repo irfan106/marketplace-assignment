a053f4cd92e3f2df4e0b599cd75dd21f10e4491a
irfan106 <ir.irfan106@gmail.com>
2024-04-08 11:44:17 +0530

    fixed port

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const Model = require("./models/Model");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
